f5d42ea956e8994202683c7d96069d7e90e26917
Billy Park <billyparkwork@gmail.com>
2016-12-09 21:26:04 +0000

    Initial commit, basic html/css/javascript in place navbar still needs work projects need to be entered contact form to be styled

---
+++ js/portfolio.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+  portfolioLine = $('#portfolioWrapper').offset().top;
+
+  $('#navbar > .button').hover(function(){
+    $(this).addClass("buttonHover");
+  })
+
+  $('#navbar > .button').mouseout(function(){
+    $(this).removeClass("buttonHover");
+  })
+  /*possible to edit css of classes instead of having different classes?*/
+  /*change to addclass for anim*/
+  $(window).scroll(function() {
+    if($('#navbar').offset().top > portfolioLine ) {
+      $('#navbar').addClass("navbarAlt");
+    } else {
+      $('#navbar').removeClass("navbarAlt");
+    }
+  })
+
+  /*$('navbar > .button').click(function(){
+    $(this).scrollTop
+  })*/
+
+});
