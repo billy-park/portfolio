d6125aaaf414e72a6061a2f1111a9306f9044193
Billy Park <billyparkweb@gmail.com>
2017-02-24 14:53:14 +0000

    Add files via upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -12,6 +12,48 @@ $(document).ready(function() {
   if ($(window).scrollTop() > navbarLine) {
       $('#navbar').addClass("navFixed");
   };
+
+  //apply button style on mouseover and remove on mouseout
+  $('#navbar > .button').hover(function(){
+    $(this).addClass("buttonHover");
+  })
+  $('#navbar > .button').mouseout(function(){
+    $(this).removeClass("buttonHover");
+  })
+
+  $(window).scroll(function(){
+    if ($(window).scrollTop() > navbarLine) {
+      $('#navbar').addClass("navFixed");
+    }
+    if ($(window).scrollTop() < navbarLine) {
+      $('#navbar').removeClass("navFixed");
+    }
+  })
+
+  //scroll to anchors
+  //credit to: https://www.abeautifulsite.net/smoothly-scroll-to-an-element-without-a-jquery-plugin-2
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top - 58
+        }, 1000, 'swing');
+    }
+  });
+  //expand/shrink project information div
+  /* removed because of potential issues with mobile devices
+  $('.projectImageContainer').hover(function(){
+    $(this).find('.projectImageOverlay').animate({height: "88px"}, 300);
+  });
+  $('.projectImageContainer').mouseout(function(){
+    $(this).find('.projectImageOverlay').animate({height: textHeightDefault});
+  });
+  */
+  /*$('navbar > .button').click(function(){
+    $(this).scrollTop
+  })*/
+
   //switch navbar classes depending on location on page
   /*if($('#navbar').offset().top < portfolioLine ) {
     $('#navbar').addClass("navbarDefault");
@@ -31,33 +73,4 @@ $(document).ready(function() {
     }
   });
 */
-  //apply button style on mouseover and remove on mouseout
-  $('#navbar > .button').hover(function(){
-    $(this).addClass("buttonHover");
-  })
-  $('#navbar > .button').mouseout(function(){
-    $(this).removeClass("buttonHover");
-  })
-
-  $(window).scroll(function(){
-    if ($(window).scrollTop() > navbarLine) {
-      $('#navbar').addClass("navFixed");
-    }
-    if ($(window).scrollTop() < navbarLine) {
-      $('#navbar').removeClass("navFixed");
-    }
-  })
-  //expand/shrink project information div
-  /* removed because of potential issues with mobile devices
-  $('.projectImageContainer').hover(function(){
-    $(this).find('.projectImageOverlay').animate({height: "88px"}, 300);
-  });
-  $('.projectImageContainer').mouseout(function(){
-    $(this).find('.projectImageOverlay').animate({height: textHeightDefault});
-  });
-  */
-  /*$('navbar > .button').click(function(){
-    $(this).scrollTop
-  })*/
-
 });
